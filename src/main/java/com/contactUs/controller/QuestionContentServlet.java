d80539c3c99821e3b27bff027cbcb4d64aca8c3e
EricLiwh <r98121010@gmail.com>
2022-09-17 10:45:24 +0800

    加了判斷，才不會一開始就出現"提問失敗"

--- src/main/java/com/contactUs/controller/QuestionContentServlet.java
+++ src/main/java/com/contactUs/controller/QuestionContentServlet.java
@@ -31,8 +31,13 @@ public class QuestionContentServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		this.doPost(req, resp);
 	}
+	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("application/json;charset=UTF-8");
+		req.setCharacterEncoding("UTF-8");
+		
 		final String questionTypeIDStr = req.getParameter("questionTypeID");
+		//第一次進入此頁，使用者尚未選擇questionTypeID，故先不進行字串轉Int
 		Integer questionTypeID = null;
 		if(!StringUtils.isNullOrEmpty(questionTypeIDStr)) {
 			questionTypeID = Integer.parseInt(questionTypeIDStr);
@@ -41,11 +46,18 @@ public class QuestionContentServlet extends HttpServlet {
 		
 		QuestionContentVO quesContent = new QuestionContentVO();
 		quesContent.setQuestionTypeID(questionTypeID);
-		quesContent.setQuestionContent(questionContent);	
-		final boolean result = service.submitQuestion(quesContent);
-		req.setAttribute("result", result? "提問已送出" : "提問失敗");
+		quesContent.setQuestionContent(questionContent);
 		
+		String quesContent1 = quesContent.getQuestionContent();	
+		if(quesContent1 != null) {
+			final boolean result = service.submitQuestion(quesContent);
+			req.setAttribute("result", result? "提問已送出" : "提問失敗");
+		}
+		
+		
+//		req.setCharacterEncoding("UTF-8");
 		req.getRequestDispatcher("/questionContent.jsp").forward(req, resp);
 	}
+	
 	
 }//class
